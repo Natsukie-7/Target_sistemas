21da21104ddb473a4359547d50383d74ad89a594
Nathan <nathanfxs17@gmail.com>
2023-04-10 13:17:03 -0300

    A resposta desde desafio é 91

---
+++ Desafios/desafio1.py
@@ -0,0 +1,21 @@
+# Observe o trecho de codigo abaixo
+
+# int INDICE = 13, SOMA = 0, K = 0
+
+# ENQUANTO K < INDICE FAÇA
+# {
+#     K = K + 1
+#     SOMA = SOMA + K
+# }
+
+# IMPRIMIR SOMA
+
+# Em resumo, é so fazer a soma usando um loop
+
+indice, soma, k = 13, 0, 0
+
+while k < indice:
+    k += 1
+    soma += k
+
+print(soma)
